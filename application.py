fcdbc4bc24174450718bedef9fdb99b95cd19246
lgmoncad <fiyugun@gmail.com>
2025-06-27 12:11:58 -0500

    Cambiando el archivo application.py

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from  flask import Flask, render_template
 
 app=Flask(__name__)
@@ -11,31 +10,3 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-=======
-from  flask import Flask, render_template
-
-app=Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html",Nombre="Carlos")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> a229e18 (Hemos Agregado una variable nombre carlos)
